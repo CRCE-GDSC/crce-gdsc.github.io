3b4b18862755303842f0708e89e10e0f7432e10d
Candida Noronha <crcegdsc@gmail.com>
2022-08-20 09:26:21 +0530

    Form changes

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -4,23 +4,15 @@ document.querySelector('.contactForm').addEventListener('submit', submitForm);
 
 // Submit form
 function submitForm(e) {
-  e.preventDefault();
+  //e.preventDefault();
 
  // document.getElementById("contactForm").reset();
   
   //document.querySelector('.contactForm').reset();
-
-}
-
-// function myResetFunction() {
-//   document.getElementById("contactForm").reset();
-// }
-
-// Alert some text when the form is reset
-function myAlertFunction() {
-  //alert("Thank you for contacting us! <br/>");
   swal("Thank You for Contacting Us", "We'll get back to you soon!", "success");
 }
+
+
 
 
 
